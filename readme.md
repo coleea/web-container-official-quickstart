9558343e01d79f8175ca0f14f0efaeb6840d6369
GUKBEOM LEE <gukbeomlee@GUKBEOMui-Macmini.local>
2023-05-02 12:41:25 +0900

    1st commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+<img src="/webcontainer_api-logo-dark-blackwhite.svg" />
+
+# tutorial url
+
+- https://webcontainers.io/tutorial/1-build-your-first-webcontainer-app
---
+++ src/main.tsx
@@ -0,0 +1,53 @@
+import { files } from './files';
+import './index.css'
+import { WebContainer } from '@webcontainer/api';
+
+let webcontainerInstance: WebContainer;
+
+const iframeEl = document.querySelector('iframe');
+const textareaEl = document.querySelector('textarea');
+
+window.addEventListener('load', async () => {
+
+  textareaEl!.value = files['index.js'].file.contents;
+
+  webcontainerInstance = await WebContainer.boot();
+
+  textareaEl && textareaEl.addEventListener('input', (e) => {
+    writeIndexJS(e.currentTarget.value)
+  })
+  
+  await webcontainerInstance.mount(files);
+
+  
+  const packageJSON = await webcontainerInstance.fs.readFile('package.json', 'utf-8');
+  const exitCode = await installDependencies(webcontainerInstance)
+  if (exitCode !== 0) {
+    throw new Error('Installation failed');
+  }
+  startDevServer(webcontainerInstance)
+});
+
+async function installDependencies(webcontainerInstance : any) {
+  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  installProcess.output.pipeTo(new WritableStream({
+    write(data) {
+      const logElem = document.querySelector('.log textarea')
+      if(logElem) logElem.textContent += '\n' + data ;
+      console.log(data);
+    }
+  }));
+  return installProcess.exit;
+}
+
+async function startDevServer(webcontainerInstance : any) {
+  await webcontainerInstance.spawn('npm', ['run', 'start']);
+
+  webcontainerInstance.on('server-ready', (port : number, url : string) => {
+    iframeEl!.src = url;
+  });
+}
+
+async function writeIndexJS(content : string) {
+  await webcontainerInstance.fs.writeFile('/index.js', content);
+}
